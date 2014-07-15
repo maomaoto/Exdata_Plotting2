1a56c8eaf351ed31464ad878494cd2da99e3c581
maomaoto <maomaoto@gmail.com>
2014-07-16 00:43:09 +0800

    Add plot1.R

---
+++ plot1.R
@@ -0,0 +1,8 @@
+## Coursera Exploratory Data Analysis
+## Course Project 2
+## plot1.R
+
+# Assume the data set is in the working directory
+# Read data and source classification code
+data <- readRDS("summarySCC_PM25.rds")
+class <- readRDS("Source_Classification_Code.rds")
